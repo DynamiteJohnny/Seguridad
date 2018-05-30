4e8d5617bad99427b11ef3f7137b0849b87cf6f2
DynamiteJohnny <a01261426@itesm.mx>
2018-05-30 17:33:54 -0500

    fin

--- functionc.c
+++ functionc.c
@@ -54,14 +54,13 @@ int main(int argc, const char * argv[]) {
                 ex = 0;
                 ex2 = 0;
                 while (ex < tam ) {
-                	if (ex2 > perm) {
+                	if (ex2 >= perm) {
                 		ex2 = 0;
                 	}
                 	if (arr1[ex] == 32) {
                 		arr3[ex] = arr1[ex];
                 		printf("\n");
                 		ex++;
-                		ex2++;
                 	}else if (arr1[ex] >= 48 && arr1[ex] <= 57) {
                 		arr3[ex] = abs(((arr2[ex2]-65) + (arr1[ex]-48)))%10;
 	                    arr3[ex] = arr3[ex]+48;
@@ -76,7 +75,49 @@ int main(int argc, const char * argv[]) {
 	                    ex2++;
 	                }
                 }
-                printf("\n");
+
+                if(tam%2 == 0){      
+				    for( int i = 0; i < (tam/2); i++){
+				        char temp = arr3[i];
+				        arr3[i] = arr3[tam - i - 1];
+				        arr3[tam - i - 1] = temp;
+				    }
+				}else{
+				    for( int i = 0; i < ((tam - 1) / 2); i++){
+				        char temp = arr3[i];
+				        arr3[i] = arr3[tam - i - 1];
+				        arr3[tam - i - 1] = temp;
+				    }
+				    
+				}
+
+
+				     
+
+			    for(int i = 0; i < tam; i++){ 
+			        for(int j = 0;j < 3;j++){ 
+			            if((arr3[i]>=65 && arr3[i]<90)){ 
+			                arr3[i]++;                
+			            }else if(arr3[i]==90){
+			            	arr3[i]=65; 
+				        }
+				    }
+
+			    } 
+
+				
+				printf("\n");
+
+                printf("\nEl texto cifrado es:");
+
+                cam = 0;
+                while(cam < tam) {
+                    printf("%c",arr3[cam]);
+                    cam++;
+                }
+
+                printf("\n\n");
+
                 break;
 
             /*Descifrar*/
@@ -115,17 +156,41 @@ int main(int argc, const char * argv[]) {
                     cam++;
                 }
 
+			    for(int i = 0;i < tam ;i++){ 
+			        for(int j = 0;j < 3;j++){ 
+			            if(arr1[i]>65 && arr1[i]<=90){ 
+			                arr1[i]--;                
+			            }else if(arr1[i]==65){
+			            	arr1[i]=90; 
+			            }
+			        } 
+			    }
+
+                if(tam%2 == 0){      
+				    for( int i = 0; i < (tam/2); i++){
+				        char temp = arr1[i];
+				        arr1[i] = arr1[tam - i - 1];
+				        arr1[tam - i - 1] = temp;
+				    }
+				}else{
+				    for( int i = 0; i < ((tam - 1) / 2); i++){
+				        char temp = arr1[i];
+				        arr1[i] = arr1[tam - i - 1];
+				        arr1[tam - i - 1] = temp;
+				    }
+				    
+				}
+
                 ex = 0;
                 ex2 = 0;
                 while (ex < tam ) {
-                	if (ex2 > perm) {
+                	if (ex2 >= perm) {
                 		ex2 = 0;
                 	}
                 	if (arr1[ex] == 32) {
                 		arr3[ex] = arr1[ex];
                 		printf("\n");
                 		ex++;
-                		ex2++;
                 	}else if (arr1[ex] >= 48 && arr1[ex] <= 57) {
                 		arr3[ex] = abs(((arr1[ex]-48+10) - (arr2[ex2]-65)))%10;
 	                    arr3[ex] = arr3[ex]+48;
@@ -141,6 +206,17 @@ int main(int argc, const char * argv[]) {
 	                }
                 }
                 printf("\n");
+
+                printf("\nEl texto cifrado es:");
+
+                cam = 0;
+                while(cam < tam) {
+                    printf("%c",arr3[cam]);
+                    cam++;
+                }
+
+                printf("\n\n");
+
                 break;
             case 3:
                 x = 0;
@@ -153,20 +229,3 @@ int main(int argc, const char * argv[]) {
 }
 
 
-/*
-if(perm2%2 == 0){
-                    
-    for( int i = 0; i < (perm2/2); i++){
-        char temp = arr2[i];
-        arr2[i] = arr2[perm2 - i - 1];
-        arr2[perm2 - i - 1] = temp;
-    }
-}else{
-    for( int i = 0; i < ((perm - 1) / 2); i++){
-        char temp = arr2[i];
-        arr2[i] = arr2[perm - i - 1];
-        arr2[perm - i - 1] = temp;
-    }
-    
-}
-*/
